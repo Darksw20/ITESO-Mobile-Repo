2fc3cc6ec12cb7ae1cae1db91163e4c9d22204fe
Darksw20 <ricardog.navi20@gmail.com>
2024-04-28 14:26:19 -0600

    Added search input fixed

--- firebase_querys/lib/home/home_page.dart
+++ firebase_querys/lib/home/home_page.dart
@@ -7,10 +7,17 @@ import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 
-class HomePage extends StatelessWidget {
-  final _fabKey = GlobalKey<ExpandableFabState>();
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  HomePage({super.key});
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _fabKey = GlobalKey<ExpandableFabState>();
+  String _searchText = "";
+  String _orderBy = "";
 
   @override
   Widget build(BuildContext context) {
@@ -30,42 +37,61 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Row(
+      body: Column(
         children: [
-          //add search input
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: TextField(
-                decoration: const InputDecoration(
-                  hintText: 'Search...',
-                  border: OutlineInputBorder(),
-                ),
-                onChanged: (value) {
-                  // Perform search based on the entered value
-                  // Update the query parameter of FirestoreListView
-                  print("Search: $value");
-                },
+          SizedBox(
+            width: 250.0, // Adjust width as needed
+            child: TextField(
+              decoration: const InputDecoration(
+                hintText: 'Search...',
+                border: OutlineInputBorder(),
               ),
+              onChanged: (value) {
+                // Implement search functionality here
+                // Update the query parameter based on value
+                print(value);
+                setState(() {
+                  _searchText = value;
+                });
+              },
             ),
           ),
-          FirestoreListView(
-            padding: EdgeInsets.symmetric(horizontal: 18),
-            pageSize: 15,
-            query: getNotesQuery(
-                "findByName", FirebaseAuth.instance.currentUser!.uid,
-                name: "test"),
-            // query: getNotesQuery("sortByDate", FirebaseAuth.instance.currentUser!.uid),
-            // query: getNotesQuery("", FirebaseAuth.instance.currentUser!.uid),
-            itemBuilder: (BuildContext context,
-                QueryDocumentSnapshot<Map<String, dynamic>> document) {
-              return ItemNote(
-                  noteContent: document.data(),
-                  onDelete: () {
-                    print("Note deleted");
-                  });
-            },
+          SizedBox(
+            width: 250.0, // Adjust width as needed
+            child: ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (_orderBy == "up") {
+                        _orderBy = "down";
+                      } else if (_orderBy == "down" || _orderBy == "") {
+                        _orderBy = "up";
+                      }
+                    });
+                  },
+                  child: Text("Order by date"),
+                ),
+              ],
+            ),
           ),
+          Flexible(
+            child: FirestoreListView(
+              padding: EdgeInsets.symmetric(horizontal: 18),
+              pageSize: 15,
+              query: getNotesQuery(FirebaseAuth.instance.currentUser!.uid,
+                  order: _orderBy, name: _searchText),
+              itemBuilder: (BuildContext context,
+                  QueryDocumentSnapshot<Map<String, dynamic>> document) {
+                return ItemNote(
+                    noteContent: document.data(),
+                    onDelete: () {
+                      print("Note deleted");
+                    });
+              },
+            ),
+          )
         ],
       ),
       floatingActionButtonLocation: ExpandableFab.location,
@@ -102,24 +128,47 @@ class HomePage extends StatelessWidget {
 }
 
 // Separated function for building the query
-Query<Map<String, dynamic>> getNotesQuery(String operation, String userId,
-    {String? name}) {
-  String collection = "tweet";
+Query<Map<String, dynamic>> getNotesQuery(String userId,
+    {String order = "up", String name = ""}) {
+  String collection = "notes";
 
-  if (operation == "findByName") {
+  print('${FirebaseAuth.instance.currentUser!.uid}, $order, $name');
+
+  if (name == "") {
+    if (order == "up") {
+      return FirebaseFirestore.instance
+          .collection(collection)
+          .where("userId", isEqualTo: userId)
+          .orderBy("createdAt");
+    } else if (order == "down") {
+      return FirebaseFirestore.instance
+          .collection(collection)
+          .where("userId", isEqualTo: userId)
+          .orderBy("createdAt", descending: true);
+    }
+    return FirebaseFirestore.instance
+        .collection(collection)
+        .where("userId", isEqualTo: userId);
+  }
+
+  if (order == "up") {
     return FirebaseFirestore.instance
         .collection(collection)
         .where("userId", isEqualTo: userId)
         .where("data.title", isGreaterThanOrEqualTo: name)
-        .where("data.title", isLessThanOrEqualTo: '${name!}\uf8ff');
-  } else if (operation == "sortByDate") {
+        .where("data.title", isLessThanOrEqualTo: '${name!}\uf8ff')
+        .orderBy("createdAt");
+  } else if (order == "down") {
     return FirebaseFirestore.instance
         .collection(collection)
         .where("userId", isEqualTo: userId)
-        .orderBy("date");
+        .where("data.title", isGreaterThanOrEqualTo: name)
+        .where("data.title", isLessThanOrEqualTo: '${name!}\uf8ff')
+        .orderBy("createdAt", descending: true);
   }
-
   return FirebaseFirestore.instance
       .collection(collection)
-      .where("userId", isEqualTo: userId);
+      .where("userId", isEqualTo: userId)
+      .where("data.title", isGreaterThanOrEqualTo: name)
+      .where("data.title", isLessThanOrEqualTo: '${name!}\uf8ff');
 }
